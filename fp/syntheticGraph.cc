42584dd3135c2841528f023aa4d79141c088a7ad
Bautista <fbautista@ada.middlebury.edu>
2025-05-10 00:34:18 -0400

    tested 500k with simd+OpenMp

--- fp/syntheticGraph.cc
+++ fp/syntheticGraph.cc
@@ -7,8 +7,8 @@ using namespace std;
 namespace fs = std::filesystem;
 
 int main() {
-    const long long NUM_EDGES = 1000000;  // 1 million edges
-    const int NUM_NODES = 100000;         // 100k unique nodes
+    const long long NUM_EDGES = 5000000;  // 5 million edges
+    const int NUM_NODES = 500000;         // 500k unique nodes
     const string OUTPUT_FILE = "synthetic_graph.txt";
 
     // If the output file already exists, we will overwrite it
